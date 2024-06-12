d53a6bfabc2df8babb80fb2895f151ae1273f378
JPODBA <joao.oliveira@ploomes.com>
2024-06-12 11:17:52 -0300

    SQL 1.1.0

--- SQLServer/Deploys/06.06.24 - Deploy ajuste particionamento Indexes - DBA/Criando Deploy - Rollback.sql
+++ SQLServer/Deploys/06.06.24 - Deploy ajuste particionamento Indexes - DBA/Criando Deploy - Rollback.sql
@@ -3,7 +3,7 @@ go
 
 Drop table if exists #DBA
 SELECT 
-    i.name AS IndexName,
+    i.name AS Index_name,
     df.name AS DataFileName,
     df.physical_name AS DataFilePath,
     SUM(s.[used_page_count]) * 8 AS IndexSizeKB
@@ -16,19 +16,36 @@ JOIN sys.dm_db_partition_stats s ON s.[object_id] = i.[object_id] AND s.[index_i
 WHERE df.type = 0  -- Data files only, not log files
 	and i.[name] NOT LIKE 'PK%'
 	AND i.[name] NOT LIKE 'UK%'
-	and df.database_id = 5
-	--and df.name = 'Ploomes_CRM_INDEX'	
+	and df.database_id = 8
 GROUP BY i.[name], df.name, df.physical_name
 ORDER BY 
     IndexSizeKB DESC;
-
 GO
+
+DROP TABLE IF EXISTS #DBA_TOTAL_ACESSO
+SELECT
+	ob.name AS Table_name,
+	id.name AS Index_name,
+	(us.user_seeks + us.user_scans + us.user_lookups) [totalAcesso]
+into #DBA_TOTAL_ACESSO
+FROM sys.dm_db_index_usage_stats us
+JOIN sys.objects  ob ON us.OBJECT_ID = ob.OBJECT_ID
+JOIN sys.indexes  id ON id.index_id = us.index_id AND us.OBJECT_ID = id.OBJECT_ID
+JOIN sys.schemas  sc ON sc.schema_id = ob.schema_id
+WHERE id.name not like 'PK%'
+	AND id.name not like 'UK%'
+ORDER BY
+		[totalAcesso] DESC
 
 --delete from #DBA where IndexSizeKB <= 9017848
 --delete from #DBA where datafileName = 'Ploomes_CRM_INDEX_02'
 --Select * from #DBA order by IndexSizeKB desc
 GO
+--SELECT * FROM #DBA_TOTAL_ACESSO ORDER BY 3 DESC
+--Delete from #DBA_TOTAL_ACESSO where totalAcesso > 1177225
 
+
+-- Cria a execução. 
 SELECT 
 	T.name as tabela,
 	I.name as IndexName, 
@@ -41,11 +58,12 @@ SELECT
 	    FOR XML PATH('')), 1, 2, '')  + ') ' +
 	ISNULL(' INCLUDE (' + IncludedColumns + ') ', '') +
 	ISNULL(' WHERE ' + I.filter_definition, '') + 
-	'WITH (FILLFACTOR = 100) ON [Ploomes_CRM_INDEX_03];' + CHAR(13) + CHAR(10) as [CreateIndex],
+	'WITH (FILLFACTOR = 100) ON [INDEX_04];' -- Alterar o Data_File
+	+ CHAR(13) + CHAR(10) as [CreateIndex],
 	'DROP INDEX IF EXISTS ['+ I.name +'] ON ['+ SCHEMA_NAME(T.schema_id) +'].['+ T.name +'];' +
 	CHAR(13) + CHAR(10) AS [DropIndex]
 FROM    sys.indexes I INNER JOIN   
-				#DBA           dba on dba.IndexName = I.name INNER JOIN  
+				#DBA_TOTAL_ACESSO           dba on dba.Index_name = I.name INNER JOIN  
         sys.tables T ON  T.object_id = I.object_id INNER JOIN       
         sys.stats ST ON  ST.object_id = I.object_id AND ST.stats_id = I.index_id INNER JOIN 
         sys.data_spaces DS ON  I.data_space_id = DS.data_space_id INNER JOIN 
@@ -65,5 +83,7 @@ FROM    sys.indexes I INNER JOIN
         ) tmp2
         ON tmp2.object_id = I.object_id AND tmp2.index_id = I.index_id
 WHERE I.is_primary_key = 0 AND I.is_unique_constraint = 0
-	and dba.IndexSizeKB <= 9017848
+	--and dba.totalAcesso < 101773
 Order by T.name
+
+--Delete from #DBA_TOTAL_ACESSO where totalAcesso < 101773
